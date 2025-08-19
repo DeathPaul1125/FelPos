2ed8215a0eacca577e6fc1cb56d1bcb10bd08a70
Pablo <civernet@gmail.com>
2025-08-19 10:27:17 -0600

    Agregando SaleDetail

---
+++ app/Models/SaleDetail.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SaleDetail extends Model
+{
+    protected $fillable = [
+        'sale_id',
+        'product_id',
+        'price',
+        'quantity',
+        'total',
+        'description',
+        'slug',
+    ];
+}
